b302f6c082ab083b080e3f49eacd7c369310a091
Maurin3 <maurin3_1994@msn.com>
2019-06-07 11:07:04 +0200

    [MOD] aac: Changed links to target correct repository (repo name changed)

--- odoo-admin-auto-connect.user.js
+++ odoo-admin-auto-connect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Odoo Admin Auto Connect
-// @icon         http://github.com/Maurin3/userscripts/blob/master/images/aac.png?raw=true
+// @icon         http://github.com/Maurin3/Userscripts/blob/master/images/aac.png?raw=true
 // @namespace    https://github.com/Maurin3
 // @version      0.1.1
 // @description  Auto connect as admin in runbots of odoo.com (and local instance)
@@ -8,8 +8,8 @@
 // @match        https://*.runbot*.odoo.com/web/login
 // @include      /^http://[0-9]{6}\-(saas\-)?[0-9]{2}\-[0-9]+\-[a-z0-9]{6}\.runbot[0-9]{2}\.odoo\.com/web/login$/
 // @match        http://localhost:8069/web/login
-// @downloadURL  https://raw.githubusercontent.com/Maurin3/userscripts/master/odoo-admin-auto-connect.user.js
-// @updateURL    https://raw.githubusercontent.com/Maurin3/userscripts/master/odoo-admin-auto-connect.user.js
+// @downloadURL  https://raw.githubusercontent.com/Maurin3/Userscripts/master/odoo-admin-auto-connect.user.js
+// @updateURL    https://raw.githubusercontent.com/Maurin3/Userscripts/master/odoo-admin-auto-connect.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
