77b52faeb09206f86a454777f1f6ab0f92601a61
Maurin3 <maurin3_1994@msn.com>
2020-06-17 21:29:04 +0200

    [DEV] har: Add userscript
    Added the 'Hide Archived Repos GitHub' userscript.

---
+++ hide-archive-repo-github.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Hide Archived Repos GitHub
+// @icon         https://github.githubassets.com/pinned-octocat.svg
+// @namespace    https://github.com/Maurin3
+// @version      1.0
+// @description  Hide archived repositories (private and public) on GitHub
+// @author       Maurin3
+// @include      https://github.com/*
+// @grant        none
+// @run-at       document-end
+// @downloadURL  https://raw.githubusercontent.com/Maurin3/Userscripts/master/dark-odoo-sh.user.js
+// @updateURL    https://raw.githubusercontent.com/Maurin3/Userscripts/master/dark-odoo-sh.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function removeArchived(){
+        if(new RegExp("^https:\/\/github\.com\/.+\\?tab=repositories$").test(document.location.href)){
+            var observer = new MutationObserver(function () {
+                let archivedRepos = document.getElementsByClassName('archived');
+                if (archivedRepos.length > 0) {
+                    // It exists now
+                    for(let repo of archivedRepos){
+                        repo.classList.add('d-none');
+                    }
+                    observer.disconnect();
+                }
+            });
+            observer.observe(document.documentElement, { childList: true, subtree: true });
+        }
+    }
+
+    window.onload = function() {
+        removeArchived();
+    }
+    window.onbeforeunload = function() {
+        removeArchived();
+    }
+})();
