53c7089a6336f6e571c2e8da1ca834587fff1a10
Maurin3 <maurin3_1994@msn.com>
2020-07-14 22:29:45 +0200

    [IMP] dsh: Moved CSS stuff into CSS file + Add button
    Moved all CSS Stuff in a CSS file to fetch from GitHub and add
    a button into the body to activate/deactivate the dark mode.

--- dark-odoo-sh.user.js
+++ dark-odoo-sh.user.js
@@ -2,324 +2,146 @@
 // @name         Dark theme for odoo.sh
 // @icon         http://github.com/Maurin3/Userscripts/blob/master/images/dsh.png?raw=true
 // @namespace    https://github.com/Maurin3
-// @version      1.2.1
+// @version      1.2.2
 // @description  Make odoo.sh dark
 // @author       Maurin3
 // @match        https://www.odoo.sh/*
 // @include      /^https://eupp[0-9]*.odoo\.com/paas/*
 // @include      /^https://eupd[0-9]*.odoo\.com/paas/*
-// @grant        none
 // @run-at       document-start
+// @grant        GM.getValue
+// @grant        GM_getValue
+// @grant        GM.setValue
+// @grant        GM_setValue
+// @grant        GM.xmlHttpRequest
+// @grant        GM_xmlhttpRequest
+// @connect      http://localhost:8000
+// @connect      raw.githubusercontent.com
 // @downloadURL  https://raw.githubusercontent.com/Maurin3/Userscripts/master/dark-odoo-sh.user.js
 // @updateURL    https://raw.githubusercontent.com/Maurin3/Userscripts/master/dark-odoo-sh.user.js
 // ==/UserScript==
 
-(function() {
+(async() => {
     'use strict';
-    var css = [
-        // General
-        "main{",
-        "    background-color: #1a1a1a;",
-        "    color: #FFFFFF;",
-        "}",
-        "#wrapwrap{",
-        "    background-color: #222222;",
-        "}",
-        "h1, h2, h3, h4, h5{",
-        "    color: #FFFFFF;",
-        "}",
-        "a:hover{",
-        "    color: #00b3b0;",
-        "}",
-        ".btn-link, a {",
-        "    color: #bfbfbf;",
-        "}",
+    var defaults = { 'dark': false };
+    let data = {};
+    var css = false;
+    // NOTE: run python to get elements available locally
+    // CMD: python -m SimpleHTTPServer
+    const isDebug = false;
 
-        // Projects Page
-        ".o_sh_projects_cards > .o_project_cards > .o_project_create_new > a > .o_project_card{",
-        "    background-color: #1a1a1a;",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_project_cards > .o_project_card_container > .o_project_card{",
-        "    background-color: #1a1a1a;",
-        "    color: #FFFFFF;",
-        "    box-shadow: 0 1px 3px 0 #808080, 0 0 0 1px #808080;",
-        "}",
-        ".o_project_title, body .text-black{",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_sh_projects_search  {",
-        "    color: initial;",
-        "}",
-        ".card {",
-        "    background-color: #383838;",
-        "    color: #FFFFFF;",
-        "}",
-        ".text-800, .btn {",
-        "    color: #FFFFFF !important;",
-        "}",
-        ".table {",
-        "    color: #dee2e6 !important;",
-        "}",
-        "hr {",
-        "    border-top: 1px solid rgba(255, 255, 255, 0.1);",
-        "}",
-        "a.text-800:hover {",
-        "    color: #dee2e6 !important;",
-        "}",
-
-        // Home Page
-        ".bg-gray-lighter > .container > div > div>  h1,.bg-gray-lighter > .container > div > div > h2, .bg-gray-lighter > .container > div > div > h3, .bg-gray-lighter > .container > div > div > h4, .bg-gray-lighter > .container > div > div > h5{",
-        "    color: #21313a;",
-        "}",
-
-        // Project/Branch History Page
-        ".o_branch_history, .o_branch_history .o_tracking_date_separator .o_tracking_date {",
-        "    background-color: #222222;",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_branch_history .o_tracking .o_tracking_commits .o_tracking_commit .o_tracking_commit_body{",
-        "    background-color: rgba(128, 128, 128, 0.25);",
-        "    white-space: break-spaces !important;",
-        "}",
-        ".o_tracking_commit_body > .o_tracking_commit_body {",
-        "    white-space: break-spaces !important;",
-        "}",
-        ".o_tracking_commits > .o_tracking_commit {",
-        "    background-color: rgba(255,255,255,0.05) !important;",
-        "}",
-        ".o_branch_history .o_sh_tracking_icon .gi {",
-        "    background-color: #222222 !important;",
-        "}",
-        ".list-group-item:not([class*='list-group-item-']):not(.active) {",
-        "    color: #FFFFFF !important;",
-        "}",
-        ".btn-light.disabled {",
-        "    color: #212529 !important;",
-        "}",
-        ".o_branch_history .o_tracking:hover {",
-        "    background-color: #4d4d4d;",
-        "    border-color: #ced4da;",
-        "}",
-        "a.bg-100:hover, a.bg-100:focus, button.bg-100:hover, button.bg-100:focus {",
-        "    background-color: rgba(255,255,255,0.7) !important;",
-        "    color: #212529 !important;",
-        "}",
-        ".dropdown-menu, .dropdown-menu a{",
-        "    background-color: #222222 !important;",
-        "    color: #FFFFFF !important;",
-        "}",
-        ".o_branch_buttons{",
-        "    background-color: #3A404A !important;",
-        "}",
-        ".js_git_command {",
-        "    background-color: #383838 !important;",
-        "    color: #FFFFFF !important;",
-        "}",
-        ".js_copy_command {",
-        "    background-color: #222222 !important;",
-        "    color: #FFFFFF !important;",
-        "    box-shadow: none !important",
-        "}",
-
-        // General Settings Page
-        ".help-block {",
-        "    background-color: #222222;",
-        "    color: #999999;",
-        "}",
-        ".o_github_search_box .input-group-btn .btn{",
-        "    background-color: #00A09D !important;",
-        "    color: #FFFFFF !important;",
-        "}",
-        ".o_branch_settings_wrapper, .o_settings_wrapper {",
-        "    background-color: #222222;",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_settings_section_body .alert-danger {",
-        "    background-color: #2e2705;",
-        "    color: #bfbfbf;",
-        "}",
-        ".o_settings_wrapper .o_github_search_result {",
-        "    background-color: #000000;",
-        "}",
-        ".o_settings_wrapper .o_github_search_result li:hover {",
-        "    background-color: rgba(0, 0, 0, 0.25);",
-        "}",
-        ".o_settings_code.btn.btn-secondary {",
-        "    background-color: #734d69;",
-        "}",
-        ".bg-white {",
-        "    background-color: #222222 !important;",
-        "    color: #FFFFFF;",
-        "}",
-        ".alert-warning {",
-        "    background-color: #2e2705;",
-        "    color: #f9e7cc;",
-        "}",
-        ".alert-info {",
-        "    background-color: #0e1f25;",
-        "    color: #d1ecf1;",
-        "}",
-
-        // Branch Settings Page
-        ".o_branch_settings_wrapper .o_settings_pricing .table tr:nth-last-child(n + 1) td, .o_settings_wrapper .o_settings_pricing .table tr:nth-last-child(n + 1) td, .list-group-item, .o_settings_wrapper .o_github_search_box .form-control  {",
-        "    background-color: #383838;",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_tracking_stage_change.small.text-uppercase{",
-        "    color: #222222;",
-        "}",
-        ".o_branch_history .js_history_item + .js_history_item{",
-        "    border-color: #222222;",
-        "}",
-        ".o_branch_history .o_tracking .o_tracking_commits .o_tracking_commit .o_tracking_commit_msg {",
-        "    white-space: normal !important;",
-        "}",
-        ".o_branch_settings, .o_branches_right_panel, .bg-100 {",
-        "    background-color: #222222 !important;",
-        "    color: #FFFFFF;",
-        "}",
-        ".btn-light {",
-        "    background-color: #383838;",
-        "    color: #FFFFFF;",
-        "}",
-
-        // Builds Page
-        ".o_builds_view .o_builds_card.o_success {",
-        "    background-color: #15250e;",
-        "}",
-        ".o_builds_view .o_builds_card.o_warning {",
-        "    background-color: #2e2705;",
-        "}",
-        ".o_builds_view .o_builds_card.o_failed {",
-        "    background-color: #240f0f;",
-        "}",
-        ".o_sh_row .o_sh_panel_right{",
-        "    background-color: #222222;",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_sh_row .o_sh_panel_left{",
-        "    background-color: #222222;",
-        "    color: #FFFFFF;",
-        "}",
-        ".o_builds_view .o_builds_card {",
-        "    border-right: 1px solid #222222;",
-        "}",
-        ".o_builds_branch {",
-        "    background-color: #222222;",
-        "}",
-        ".bg-200 {",
-        "    background-color: #222222 !important;",
-        "    color: #FFFFFF;",
-        "}",
-        ".card-body {",
-        "    background-color:  #383838 !important;",
-        "    color: #FFFFFF;",
-        "}",
-        ".bg-white-75 {",
-        "    background-color: #4d4d4d !important;",
-        "    color: #FFFFFF;",
-        "}",
-        ".bg-white-75 {",
-        "    background-color: #222222 !important;",
-        "    color: #FFFFFF;",
-        "}",
-
-
-        // Create Project Page
-        ".o_location .panel label.control-label {",
-        "    color: #bfbfbf;",
-        "}",
-        ".panel, .panel .thumbnail, .panel .form-control {",
-        "    background-color: #222222;",
-        "    color: #bfbfbf;",
-        "}",
-        ".well {",
-        "    background-color: rgba(0,0,0,0.5);",
-        "}",
-        ".form-control option, .form-control{",
-        "    background-color: #383838;",
-        "    color: #bfbfbf !important;",
-        "}",
-        ".form-control:focus{",
-        "    background-color: #383838;",
-        "    color: #bfbfbf !important;",
-        "}",
-        ".text-700{",
-        "    color: #bfbfbf !important;",
-        "}",
-
-        // Branch Logs Page
-        ".o_sh_log_form{",
-        "    background-color: #222;",
-        "}",
-        ".o_sh_iframe_tab,",
-        "body,",
-        ".o_sh_iframe_tab body{",
-        "    background-color: #222222 !important;",
-        "}",
-        ".header{",
-        "    background-color: #222222;",
-        "    border-bottom: 2px solid #222222;",
-        "    box-shadow: 0 0 6px #555;",
-        "    color:#dfdfdf;",
-        "}",
-        ".o-logs{",
-        "    background-color: #222222;",
-        "    color: #dfdfdf;",
-        "}",
-
-        // Branch Mail Page
-        ".paas-worker-mail,",
-        ".paas-worker-mail .paas-worker-messages-container,",
-        ".paas-worker-header .navbar-default,",
-        ".paas-worker-table-messages {",
-        "    background-color: #222222;",
-        "    color: #dfdfdf;",
-        "    border-color : #222222;",
-        "}",
-        ".table.table.table-condensed.paas-worker-table-messages{",
-        "    box-shadow: 0 0 6px #555;",
-        "}",
-        ".table-striped > tbody > tr:nth-of-type(odd) {",
-        "    background-color: rgba(0, 0, 0, 0.25);",
-        "}",
-
-        // Status Page
-        ".o_sh_status .o_sh_server_lists_container tbody > tr:first-child td:first-child  {",
-        "    color: #efefef;",
-        "}",
-        ".o_sh_value.h1.text-default {",
-        "    color: #efefef;",
-        "}",
-        ".o_sh_status .o_sh_global_data_container {",
-        "    background-color: #222222;",
-        "}",
-        // Modals
-        ".modal-header,",
-        ".modal-title,",
-        ".modal-body,",
-        ".modal-footer {",
-        "    background-color: #222222 !important;",
-        "    color: #dfdfdf !important;",
-        "}",
-    ].join("\n");
-if (typeof GM_addStyle != "undefined") {
-    GM_addStyle(css);
-} else if (typeof PRO_addStyle != "undefined") {
-    PRO_addStyle(css);
-} else if (typeof addStyle != "undefined") {
-    addStyle(css);
-} else {
-    var node = document.createElement("style");
-    node.type = "text/css";
-    node.appendChild(document.createTextNode(css));
-    var heads = document.getElementsByTagName("head");
-    if (heads.length > 0) {
-        heads[0].appendChild(node);
-    } else {
-        // no head yet, stick it whereever
-        document.documentElement.appendChild(node);
+    function setStyle(element) {
+        element.style.color = 'rgba(255, 255, 255, 0.8)';
+        element.style.padding = '10px 10px';
+        element.onpointerover = () => {element.style.color = 'rgba(255, 255, 255, 1)'};
+        element.onpointerout = () => {element.style.color = 'rgba(255, 255, 255, 0.8)'};
     }
-}
+
+    async function getData() {
+        data = await GM.getValue("data", defaults);
+        try {
+            data = JSON.parse(data);
+            if (!Object.keys(data).length || ({}).toString.call(data) !== "[object Object]") {
+                throw new Error();
+            }
+        }
+        catch (err) {
+            data = await GM.getValue("data", defaults);
+        }
+    }
+
+    async function init() {
+        await retrieveCss();
+        let darkMode = document.getElementsByClassName('o_dark_mode');
+        if (darkMode.length == 0) {
+            let darkMode = document.createElement('div');
+            darkMode.classList.add('o_dark_mode');
+            let clickable = document.createElement('span');
+            setStyle(clickable);
+            await getData();
+            if (!data.dark) {
+                clickable.classList.add('fa', 'fa-moon-o');
+            }
+            else {
+                clickable.classList.add('fa', 'fa-sun-o');
+            }
+            var observer = new MutationObserver(function () {
+                let navbar = document.getElementsByClassName('navbar');
+                if (navbar.length > 0) {
+                    // It exists now
+                    let elemMenu = document.getElementsByClassName('o_user_menu');
+                    darkMode.appendChild(clickable);
+                    navbar[0].insertBefore(darkMode, elemMenu[0]);
+                    observer.disconnect();
+                }
+            });
+            observer.observe(document.documentElement, { childList: true, subtree: true });
+            clickable.addEventListener('click', addCss, false);
+            if (data.dark) {
+                dark(clickable);
+            }
+        }
+    }
+
+    window.onload = await init();
+
+    function dark(clickable) {
+        clickable.classList.remove('fa-moon-o');
+        clickable.classList.add('fa-sun-o');
+        var node = document.createElement("style");
+        node.setAttribute('id', 'style');
+        node.type = "text/css";
+        node.appendChild(document.createTextNode(css));
+        var heads = document.getElementsByTagName("head");
+        if (heads.length > 0) {
+            heads[0].appendChild(node);
+        } else {
+            // no head yet, stick it whereever
+            document.documentElement.appendChild(node);
+        }
+    }
+
+    function light(clickable) {
+        clickable.classList.remove('fa-sun-o');
+        clickable.classList.add('fa-moon-o');
+        var node = document.getElementById('style');
+        var heads = document.getElementsByTagName("head");
+        if (heads.length > 0) {
+            if (node) heads[0].removeChild(node);
+        } else {
+            // no head yet, stick it whereever
+            if (node) document.documentElement.removeChild(node);
+        }
+    }
+
+    async function addCss(ev) {
+        var clickable = document.getElementsByClassName('o_dark_mode')[0].lastChild;
+        await getData();
+        if (!data.dark || clickable.classList.contains('fa-moon-o')) {
+            dark(clickable);
+            await GM.setValue("data", JSON.stringify({ 'dark': true }));
+        }
+        else {
+            light(clickable);
+            await GM.setValue("data", JSON.stringify({ 'dark': false }));
+        }
+    }
+
+    async function retrieveCss() {
+        var root = "";
+        if (isDebug){
+            root = "http://localhost:8000/css/";
+        }
+        else{
+            root = "https://raw.githubusercontent.com/Maurin3/Userscripts/master/css/";
+        }
+        console.info(`Fetching ${root}dsh.css`);
+        await GM.xmlHttpRequest({
+            method: "GET",
+            url: `${root}dsh.css`,
+            onload: response => {
+                css = response.responseText;
+            }
+        });
+    }
 })();
