2e6ae48b98e7fbf971563a402177e18f03e8fa22
Maurin3 <maurin3_1994@msn.com>
2021-03-05 21:31:35 +0100

    [FIX] aac: Fix regex for runbots

--- odoo-admin-auto-connect.user.js
+++ odoo-admin-auto-connect.user.js
@@ -2,10 +2,10 @@
 // @name         Odoo Admin Auto Connect
 // @icon         http://github.com/Maurin3/Userscripts/blob/master/images/aac.png?raw=true
 // @namespace    https://github.com/Maurin3
-// @version      1.1.2
+// @version      1.1.3
 // @description  Auto connect as admin in runbots of odoo.com (and local instance)
 // @author       Maurin3
-// @include      ^http(s)?:\/\/[0-9]{5,}\-(saas\-)?[0-9]{2}\-[0-9]+\-[a-z0-9]{5,}\.runbot[0-9]{2}\.odoo\.com(\/)?[a-z]{0,2}(_)?[A-Z]{0,2}\/web\/login*$/
+// @include      /^http(s)?:\/\/[0-9]{5,}\-(saas\-)?[0-9]{2}\-[0-9]+(\-all)?\.runbot[0-9]{2}\.odoo\.com(\/)?[a-z]{0,2}(_)?[A-Z]{0,2}\/web\/login(\?debug=)?$/
 // @include      /^http(s)?:\/\/localhost(:)?[0-9]{0,5}(\/)?[a-z]{0,2}(_)?[A-Z]{0,2}\/web\/login*$/
 // @downloadURL  https://raw.githubusercontent.com/Maurin3/Userscripts/master/odoo-admin-auto-connect.user.js
 // @updateURL    https://raw.githubusercontent.com/Maurin3/Userscripts/master/odoo-admin-auto-connect.user.js
