d73a73cffbefa6ea5bf913ceab6393075cd17a99
Antonis Maurine <maurin3_1994@msn.com>
2025-03-18 08:21:27 +0100

    [FIX] oqc: Fix regex for runbots
    Fix the regex for rubots to allow the userscript to run correctly and target the correct URL.

--- odoo-quick-connect.user.js
+++ odoo-quick-connect.user.js
@@ -2,10 +2,10 @@
 // @name         Odoo Quick Connect
 // @icon         http://github.com/Maurin3/Userscripts/blob/master/images/oqc.png?raw=true
 // @namespace    https://github.com/Maurin3
-// @version      2.1.1
+// @version      2.1.2
 // @description  Select the impersonation in runbots of odoo.com (and local instance)
 // @author       Maurin3
-// @include      /^http(s)?:\/\/[0-9]{5,}\-((saas\-)?[0-9]{2}|master)(\-[0-9]{1})?(\-all)?\.runbot[0-9]{2,}\.odoo\.com(\/)?[a-z]{0,2}(_)?[A-Z]{0,2}\/web\/login(\?debug=)?$/
+// @include      /^http(s)?:\/\/[0-9]{5,}\-((saas\-)?[0-9]{2}|master)(\-[0-9]{1})?(\-all|)?(\-design-theme)?\.runbot[0-9]{3,}\.odoo\.com(\/)?[a-z]{0,2}(_)?[A-Z]{0,2}\/web\/login(\?debug=)?/
 // @include      /^http(s)?:\/\/localhost(:)?[0-9]{0,5}(\/)?[a-z]{0,2}(_)?[A-Z]{0,2}\/web\/login(\?debug=)?/
 // @downloadURL  https://raw.githubusercontent.com/Maurin3/Userscripts/master/odoo-quick-connect.user.js
 // @updateURL    https://raw.githubusercontent.com/Maurin3/Userscripts/master/odoo-quick-connect.user.js
